9e1205d3555321a354c5368c4b9bbe3f88d994e1
ZahraTavakoli <100377681+ZAHRATAVAKoLE@users.noreply.github.com>
2022-02-27 19:43:51 +0330

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 26 18:18:20 2022
+
+@author: zahratavakoli email (zahratavakoli261@gmail.com)
+"""
+
+k=float(input("enter your kilo: "))
+p=k*2.2046
+print("kilo convert to pond is:",p)	
